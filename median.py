69aaa7fda32e6bae32150b66acdc172acab1a76d
Mayowa Ogundele <mayowa.ogundele@kcl.ac.uk>
2024-09-27 16:36:36 +0100

    add median function

--- median.py
+++ median.py
@@ -1,6 +1,14 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def median(nums):
+    n = len(nums)
+    nums.sort()
+    if n % 2 == 1:
+        return nums[int((n+1/2) - 1)]
+    else:
+        return (nums[int(n/2 - 1)] + nums[int((n + 2)/2 - 1)]) / 2
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -9,4 +17,8 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
+    
 print(numbers)
+print(median(numbers))
+
+
